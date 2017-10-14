030dbbb9eec732e636b23558fa535671dfdb5435
bkfitz523 <bkfitz523@gmail.com>
2017-10-14 15:17:19 -0400

    Start DB and load Type, Region, Nature

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from pokemon.models import Type, Region, Nature
 
 # Register your models here.
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    ordering = ('id',)
+
+admin.site.register(Type, TypeAdmin)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    ordering = ('id',)
+
+admin.site.register(Region, RegionAdmin)
+
+
+class NatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'increased_stat',
+        'decreased_stat',
+        'favorite_flavor',
+        'disliked_flavor'
+    )
+    ordering = ('id',)
+
+admin.site.register(Nature, NatureAdmin)
--- pokemon/models.py
+++ pokemon/models.py
@@ -23,7 +23,6 @@ class Type(models.Model):
     DARK = 'Dark'
     FAIRY = 'Fairy'
 
-
     TYPE_CHOICES = (
         (NORMAL, 'Normal'),
         (FIGHTING, 'Fighting'),
@@ -47,3 +46,40 @@ class Type(models.Model):
 
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=64, choices=TYPE_CHOICES, default=NORMAL)
+
+
+class Region(models.Model):
+    KANTO = 'Kanto'
+    JOHTO = 'Johto'
+    HOENN = 'Hoenn'
+    SINNOH = 'Sinnoh'
+    UNOVA = 'Unova'
+    KALOS = 'Kalos'
+    ALOLA = 'Alola'
+
+    REGION_CHOICES = (
+        (KANTO, 'Kanto'),
+        (JOHTO, 'Johto'),
+        (HOENN, 'Hoenn'),
+        (SINNOH, 'Sinnoh'),
+        (UNOVA, 'Unova'),
+        (KALOS, 'Kalos'),
+        (ALOLA, 'Alola')
+    )
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64, choices=REGION_CHOICES, default=KANTO)
+
+    # region = models.CharField(max_length=50, choices=REGION_CHOICES, default=KANTO)
+
+    def __str__(self):
+        return self.name
+
+
+class Nature(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    increased_stat = models.CharField(max_length=50, blank=True)
+    decreased_stat = models.CharField(max_length=50, blank=True)
+    favorite_flavor = models.CharField(max_length=50, blank=True)
+    disliked_flavor = models.CharField(max_length=50, blank=True)
