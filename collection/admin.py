a6e49212e69b9669893bcdc963b0aedb9b8cfe32
bkfitz523 <bkfitz523@gmail.com>
2018-01-27 14:21:48 -0500

    Create Type table and populate data

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Import models below
+from collection.models import Type
+
+
+# Setup
+class TypeAdmin(admin.ModelAdmin):
+    model = Type
+    list_display = ('id', 'name',)
+    readonly_fields = ('id', 'name')
+    ordering = ('id',)
+
+
+# Register models
+admin.site.register(Type, TypeAdmin)
